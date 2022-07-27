13db9eab46ef0aa0950227c0f549cc5ca3f49afd
qier <1323287928@qq.com>
2022-07-27 20:36:00 +0800

    完成了分类功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {Button,Form,FormItem,Input,Message,Aside,Main,Header,
     Container,Menu,Submenu,MenuItemGroup,MenuItem,Breadcrumb,
     BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,
-    Pagination,Dialog,MessageBox,Tag,Tree,Select,Option} from "element-ui"
+    Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader} from "element-ui"
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -30,5 +30,6 @@ Vue.use(Pagination)
 Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Tree)
+Vue.use(Cascader)
 Vue.prototype.$message=Message
 Vue.prototype.$confirm=MessageBox.confirm
