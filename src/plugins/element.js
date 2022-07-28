68d6d2341bd157415db1583ab8910a861ecc476a
qier <1323287928@qq.com>
2022-07-28 21:32:29 +0800

    完成了分类参数的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import {Button,Form,FormItem,Input,Message,Aside,Main,Header,
     Container,Menu,Submenu,MenuItemGroup,MenuItem,Breadcrumb,
     BreadcrumbItem,Card,Row,Col,Table,TableColumn,Switch,Tooltip,
-    Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader} from "element-ui"
+    Pagination,Dialog,MessageBox,Tag,Tree,Select,Option,Cascader,Alert,Tabs,TabPane} from "element-ui"
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -31,5 +31,8 @@ Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Tree)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.prototype.$message=Message
 Vue.prototype.$confirm=MessageBox.confirm
